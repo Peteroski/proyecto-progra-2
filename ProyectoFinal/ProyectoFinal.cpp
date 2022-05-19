81421330d2a869bd193bde96bacea553a1d72c39
Peteroski <pedro.velez@utp.edu.co>
2022-05-19 16:50:59 -0500

    Update

---
+++ ProyectoFinal/ProyectoFinal.cpp
@@ -0,0 +1,12 @@
+﻿// ProyectoFinal.cpp : Defines the entry point for the application.
+//
+
+#include "ProyectoFinal.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
